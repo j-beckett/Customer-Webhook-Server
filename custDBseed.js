48597883bc728700198d15247db6cf81fe3daeaa
Jen Beckett <jen@rfdmsolutions.com>
2022-08-02 14:26:25 -0700

    make it better

---
+++ custDBseed.js
@@ -0,0 +1,101 @@
+//this file is for insert / update to PG database.
+//currently does not work
+
+
+//this imports the postgres connector into the file so it can be used
+const { dotenvConfig } = require('custom-env');
+const { Pool } = require('pg');
+
+//imports the AWS Postgres DB credentials from the .env file 
+require('custom-env').env();
+
+//instantiate pool to connect to the database, connection settings are passed in
+const pool = new Pool({
+    user: process.env.POSTGRESQL_DB_USER,
+    host: process.env.POSTGRESQL_DB_HOST,
+    database: process.env.POSTGRESQL_DB,
+    password: process.env.POSTGRESQL_DB_PASSWORD,
+    port: process.env.POSTGRESQL_DB_PORT
+});
+
+
+const TABLE_NAME = "public.\"node_customers_test\"";
+//
+
+async function insertData(custData, pool){
+
+    //Establish a new client. Don't forget to free the client with release() afterwards !
+    const client = await pool.connect();
+
+    try{
+        //this try block does the actual query to the PG DB
+
+        //assumption: this fits in the registered name to billing first name / last name. could be changed
+        try{
+            const response = await client.query(
+                "INSERT INTO public.\"node_customers_test\" (\"WooCustomerId\", \"Email\", \"FirstName\", \"LastName\", treez_customer_id, \"BillingAddress_FirstName\", \"BillingAddress_LastName\", \"BillingAddress_State\", \"BillingAddress_City\", \"BillingAddress_Address1\", \"BillingAddress_Address2\" , \"BillingAddress_Postcode\", \"BillingAddress_Email\", \"OriginalPlatform\", \"VerificationStatus\", gender, birthdate, banned, drivers_license_number, drivers_license_expiration, permit_expiration, warning_1, warning_2, status, nickname, notes, membership_details  ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27) ON CONFLICT (treez_customer_id) DO UPDATE SET \"WooCustomerId\" = EXCLUDED.\"WooCustomerId\" , \"Email\" = EXCLUDED.\"Email\" , \"FirstName\" = EXCLUDED.\"FirstName\" , \"LastName\" = EXCLUDED.\"LastName\" , \"Role\"  = EXCLUDED.\"Role\" , \"Username\"  = EXCLUDED.\"Username\"  " ,
+                custData
+            );
+
+            console.log(response);
+        }
+
+        catch (err) {
+            console.log("error upserting into the PG table")
+            throw err;
+        }
+
+        console.log("Inserted or Updated a Customer into the table.")
+    }
+
+    catch(err){
+        console.log(err.stack);
+        console.log("Error starting a new client.");
+
+    }
+
+    finally{
+        //free the client even if there was another error within the error handling. 
+        client.release();
+    }
+   
+
+    // str = "public.\"Node_Products_Test\"";
+    // const response = apool.query(
+    //     "SELECT * FROM " + str
+    // );
+
+   //console.log(response.rows);
+    
+
+    
+    //client.release();
+    //pool.end()
+    // .then(() => console.pool has disconnected'))
+    // .catch(err => console.error('error during disconnection', err.stack));
+    //pool.done();
+}
+
+connectToDB = async (product) => {
+
+    console.log(product);
+    try {
+        pool.connect();
+        console.log("Connected Successfully");
+
+        await insertData(product, pool);
+
+    } catch (err) {
+
+        console.log("Failed to Connect to the Pool");
+        throw err;
+        //error message
+    }
+
+
+};
+
+//this is used for running tests before connecting this file to the server (index.js)
+ //connectToDB([123,"testDB", "testingUPSERT2", "jen industries", "fixed", 123] );
+
+module.exports = {connectToDB};
---
+++ customerClient.js
@@ -0,0 +1,330 @@
+const axios = require('axios');
+
+const sleep = require('util').promisify(setTimeout)
+let i = 1;
+
+let num = 0;
+
+function myFunc() {
+    const custArr = [
+      {
+        "resultCode": "SUCCESS",
+        "resultReason": null,
+        "resultDetail": null,
+        "data": {
+            "status": "ACTIVE",
+            "verification_status": "VERIFIED",
+            "verification_reasons": [],
+            "customer_id": "15111",
+            "first_name": "testy",
+            "middle_name": "",
+            "nickname": "",
+            "last_name": "heroku",
+            "birthday": "1992-04-07",
+            "drivers_license": "C1234567",
+            "drivers_license_expiration": "2022-04-07",
+            "state_medical_id": "N/A",
+            "permit_expiration": "N/A",
+            "email": "sample@test.com",
+            "phone": "6282032916",
+            "notes": "loyal customer\nprefers flower - indica dominant hybrids",
+            "physician_first_name": null,
+            "physician_last_name": null,
+            "physician_license": null,
+            "physician_address": null,
+            "physician_phone": null,
+            "is_caregiver": false,
+            "caregiver_license_number": "",
+            "caregiver_name_1": "N/A",
+            "caregiver_name_2": "N/A",
+            "caregiver_details": [],
+            "rewards_balance": 275,
+            "rewards_type": "POINTS",
+            "gender": "M",
+            "signup_date": "2019-04-10T23:44:07.000-07:00",
+            "last_visit_date": "2020-04-23 07:42:29.000",
+            "last_update": "2020-05-09T09:41:43.000-07:00",
+            "opt_out": null,
+            "referral_source": "Other",
+            "banned": false,
+            "warning_1": false,
+            "warning_2": false,
+            "addresses": [
+                {
+                    "type": "address_of_record",
+                    "street1": "1611 TELEGRAPH AVE",
+                    "street2": "",
+                    "city": "OAKLAND",
+                    "state": "CA",
+                    "zipcode": "94612",
+                    "primary": true
+                },
+                {
+                    "type": "alternative_address",
+                    "street1": "7000 COLISEUM WAY",
+                    "street2": "",
+                    "city": "OAKLAND",
+                    "state": "CA",
+                    "zipcode": "94621",
+                    "primary": false
+                },
+                {
+                    "type": "secondary_address",
+                    "street1": "9500 STERN AVE",
+                    "street2": "",
+                    "city": "OAKLAND",
+                    "state": "CA",
+                    "zipcode": "94605",
+                    "primary": false
+                }
+            ],
+            "merged_customer_ids": [],
+            "merged_into_customer_id": null,
+            "patient_type": "ADULT",
+            "customer_groups": [
+                "INDUSTRY"
+            ]
+        }
+        },
+        {
+        "resultCode": "SUCCESS",
+        "resultReason": null,
+        "resultDetail": null,
+        "data": {
+            "status": "ACTIVE",
+            "verification_status": "VERIFIED",
+            "verification_reasons": [],
+            "customer_id": "1531",
+            "first_name": "Jen",
+            "middle_name": "",
+            "nickname": "",
+            "last_name": "beckett",
+            "birthday": "1994-01-01",
+            "drivers_license": "J1234567",
+            "drivers_license_expiration": "2022-12-12",
+            "state_medical_id": "N/A",
+            "permit_expiration": "N/A",
+            "email": "sample@test.com",
+            "phone": "6282032916",
+            "notes": "loyal customer\nprefers flower - indica dominant hybrids",
+            "physician_first_name": null,
+            "physician_last_name": null,
+            "physician_license": null,
+            "physician_address": null,
+            "physician_phone": null,
+            "is_caregiver": false,
+            "caregiver_license_number": "",
+            "caregiver_name_1": "N/A",
+            "caregiver_name_2": "N/A",
+            "caregiver_details": [],
+            "rewards_balance": 275,
+            "rewards_type": "POINTS",
+            "gender": "M",
+            "signup_date": "2019-04-10T23:44:07.000-07:00",
+            "last_visit_date": "2020-04-23 07:42:29.000",
+            "last_update": "2020-05-09T09:41:43.000-07:00",
+            "opt_out": null,
+            "referral_source": "Other",
+            "banned": false,
+            "warning_1": false,
+            "warning_2": false,
+            "addresses": [
+                {
+                    "type": "address_of_record",
+                    "street1": "1611 TELEGRAPH AVE",
+                    "street2": "",
+                    "city": "OAKLAND",
+                    "state": "CA",
+                    "zipcode": "94612",
+                    "primary": true
+                },
+                {
+                    "type": "alternative_address",
+                    "street1": "7000 COLISEUM WAY",
+                    "street2": "",
+                    "city": "OAKLAND",
+                    "state": "CA",
+                    "zipcode": "94621",
+                    "primary": false
+                },
+                {
+                    "type": "secondary_address",
+                    "street1": "9500 STERN AVE",
+                    "street2": "",
+                    "city": "OAKLAND",
+                    "state": "CA",
+                    "zipcode": "94605",
+                    "primary": false
+                }
+            ],
+            "merged_customer_ids": [],
+            "merged_into_customer_id": null,
+            "patient_type": "ADULT",
+            "customer_groups": [
+                "INDUSTRY"
+            ]
+        }
+        },
+        {
+            "event_type": "CUSTOMER",
+            "data": {
+                "status": "ACTIVE",
+                "verification_status": "VERIFIED",
+                "verification_reasons": [],
+                "customer_id": "26142",
+                "first_name": "GLEN",
+                "middle_name": null,
+                "nickname": "",
+                "last_name": "SCOTLAND",
+                "birthday": "1954-10-15",
+                "drivers_license": "E0932916",
+                "drivers_license_expiration": "2024-10-15",
+                "state_medical_id": "N/A",
+                "permit_expiration": "N/A",
+                "email": "GLENSCOTLAND@YAHOO.COM",
+                "phone": "8055092149",
+                "notes": "N/A",
+                "physician_first_name": null,
+                "physician_last_name": null,
+                "physician_license": null,
+                "physician_address": null,
+                "physician_phone": null,
+                "is_caregiver": false,
+                "caregiver_license_number": "",
+                "caregiver_name_1": "N/A",
+                "caregiver_name_2": "N/A",
+                "caregiver_details": [],
+                "rewards_balance": 290,
+                "rewards_type": "POINTS",
+                "gender": "U",
+                "signup_date": "2021-05-31T18:32:50.000-07:00",
+                "last_visit_date": "2022-08-02 12:28:56.493",
+                "last_update": "2022-08-02T12:28:57.000-07:00",
+                "opt_out": null,
+                "referral_source": null,
+                "banned": false,
+                "warning_1": false,
+                "warning_2": false,
+                "addresses": [{
+                    "type": "address_of_record",
+                    "street1": "",
+                    "street2": "",
+                    "city": "",
+                    "state": "CA",
+                    "zipcode": "",
+                    "primary": true
+                }, {
+                    "type": "secondary_address",
+                    "street1": "8899 Halifax St",
+                    "street2": "",
+                    "city": "Ventura",
+                    "state": "CA",
+                    "zipcode": "93004",
+                    "primary": false
+                }],
+                "merged_customer_ids": [],
+                "merged_into_customer_id": null,
+                "patient_type": "ADULT",
+                "customer_groups": ["SENIOR", "VET"]
+            }
+        },
+        {
+            "event_type": "CUSTOMER",
+            "data": {
+                "status": "ACTIVE",
+                "verification_status": "VERIFIED",
+                "verification_reasons": [],
+                "customer_id": "33949",
+                "first_name": "KAREN",
+                "middle_name": null,
+                "nickname": "KALE",
+                "last_name": "STARBIRD",
+                "birthday": "1942-07-04",
+                "drivers_license": "P0198852",
+                "drivers_license_expiration": "2023-07-04",
+                "state_medical_id": "N/A",
+                "permit_expiration": "N/A",
+                "email": "kalestar@aol.com",
+                "phone": "8057985583",
+                "notes": "N/A",
+                "physician_first_name": null,
+                "physician_last_name": null,
+                "physician_license": null,
+                "physician_address": null,
+                "physician_phone": null,
+                "is_caregiver": false,
+                "caregiver_license_number": "",
+                "caregiver_name_1": "N/A",
+                "caregiver_name_2": "N/A",
+                "caregiver_details": [],
+                "rewards_balance": 31,
+                "rewards_type": "POINTS",
+                "gender": "F",
+                "signup_date": "2022-04-03T13:00:46.000-07:00",
+                "last_visit_date": "2022-08-01 12:07:51.000",
+                "last_update": "2022-08-01T12:07:51.000-07:00",
+                "opt_out": "false",
+                "referral_source": "Word of mouth / friend",
+                "banned": false,
+                "warning_1": false,
+                "warning_2": false,
+                "addresses": [{
+                    "type": "address_of_record",
+                    "street1": "11816 SILVER SPUR ST",
+                    "street2": "",
+                    "city": "OJAI",
+                    "state": "CA",
+                    "zipcode": "93023",
+                    "primary": true
+                }],
+                "merged_customer_ids": [],
+                "merged_into_customer_id": null,
+                "patient_type": "ADULT",
+                "customer_groups": ["SENIOR", " INDUSTRY"]
+            }
+        }
+    ]
+
+    axios.post('http://localhost:3001/customer', custArr[num])
+        .then(function (response) {
+            console.log(response.status);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+        i++;
+  };
+
+//     setTimeout(() => {console.log("loop start. i is:" + i);
+//     let topost;
+//     if (i%10 === 0 ){
+//         topost = { product_id: 'TEST', category_type: 'sushi', product_configurable_fields: {name: "rainbowRoll", brand: "Jens Best"}, pricing: {price_type: "fixed", price_sell: 420} } ;
+//     }
+//     else {
+//         topost = prodArr[i%3];
+//     }
+
+//         axios.post('http://localhost:3001/product', topost)
+//         .then(function (response) {
+//             console.log(response.status);
+//         })
+//         .catch(function (error) {
+//             console.log(error);
+//         });
+
+//         i++;
+
+//     }, 1500);
+    
+// };
+
+(async () => {
+    for (let i = 0; i< 100; i++){
+        console.time("Slept for")
+        await sleep(2000)
+        console.timeEnd("Slept for")
+        myFunc();
+    }
+}
+)();
