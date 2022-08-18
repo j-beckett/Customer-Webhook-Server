12e3d5da625115f9fabf869d5bdfae8eafc4af77
Jen Beckett <jen@rfdmsolutions.com>
2022-08-18 11:26:02 -0700

    added some code that should hopefully prevent crashes when users do not have address data

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function formatIt(object) {
   ];
   //
 
+  if (object.addresses[0] === undefined){
+      object.addresses[0].state = null;      
+      object.addresses[0].city = null; 
+      object.addresses[0].street1 = null;  
+      object.addresses[0].street2 = null; 
+      object.addresses[0].zipcode = null;  
+  }
+
   const customFields = JSON.stringify(customField);
 
     const itemForDB = [
